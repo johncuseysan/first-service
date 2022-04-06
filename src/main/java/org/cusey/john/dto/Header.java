017c2527aaa59e2277913d359465bc97afa33d8d
johncuseysan <johncuseysan@gmail.com>
2022-04-06 11:01:31 -0400

    init commit

--- src/main/java/org/cusey/john/dto/Header.java
+++ src/main/java/org/cusey/john/dto/Header.java
@@ -2,15 +2,46 @@ package org.cusey.john.dto;
 
 public class Header {
 	
-	public int collegeId;
-
+	public String token;					//1
+	public String tokenIP; 					//2
+	public String application;				//3 Allowed values: A-Z, 0-9, a-z Min and Max length is 2
+	public int collegeId;					//4 UUID Format
+	public String data;						//5
+	
+	
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public String getTokenIP() {
+		return tokenIP;
+	}
+	public void setTokenIP(String tokenIP) {
+		this.tokenIP = tokenIP;
+	}
+	public String getApplication() {
+		return application;
+	}
+	public void setApplication(String application) {
+		this.application = application;
+	}
 	public int getCollegeId() {
 		return collegeId;
 	}
-
 	public void setCollegeId(int collegeId) {
 		this.collegeId = collegeId;
 	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+
+
 	
 	
 
